53be128818f68e97729630340113e26d9314fa28
Denis Ivanchenko <sicknature24@yandex.com>
2024-10-03 14:10:21 +0300

    feat: add solution for src/hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import crypto from 'crypto';
+import fs from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hashSum = crypto.createHash('sha256');
+    const stream = fs.createReadStream('./files/fileToCalculateHashFor.txt');
+
+    stream.on('data', (chunk) => {
+        hashSum.update(chunk);
+    })
+
+    stream.on('end', () => {
+        console.log(hashSum.digest('hex'));
+    })
 };
 
 await calculateHash();
