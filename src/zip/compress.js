85d5e9d9a454e977d1664a433082110ca36c5252
Denis Ivanchenko <sicknature24@yandex.com>
2024-10-04 23:11:11 +0300

    feat: add solution for src/zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { createGzip } from 'node:zlib';
+import fs from 'node:fs';
+import { pipeline } from 'stream';
+
+const gzip = createGzip();
+const source = fs.createReadStream('./files/fileToCompress.txt');
+const destination = fs.createWriteStream('./files/archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        pipeline(
+            source,
+            gzip,
+            destination,
+            err => {
+                console.log(err);
+                process.exitCode = 1;
+            }
+        );
+        console.log('File successfully compressed');
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import {  createGunzip } from 'node:zlib';
+import fs from 'node:fs';
+import { pipeline } from 'stream';
+
+const gunzip = createGunzip();
+const source = fs.createReadStream('./files/archive.gz');
+const destination = fs.createWriteStream('./files/decompressedFile.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        pipeline(
+            source,
+            gunzip,
+            destination,
+            err => {
+                console.log(err);
+                process.exitCode = 1;
+            }
+        )
+        console.log('File successfully decompressed');
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await decompress();
